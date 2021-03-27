f723b8146d6bf4c4346f8f47fc20f863a8cfa18d
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-03-27 17:36:14 +0000

    make it better

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,25 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+# SimpleCov.start 'rails' # lets only uncomment this if we need to
+SimpleCov.start
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-require_relative "../config/environment"
-require "rails/test_help"
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
 
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+    # Add more helper methods to be used by all tests here...
+  end
 end
